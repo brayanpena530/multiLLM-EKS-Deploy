9f49dee5e26c894014f11770af50e5d8107b4b7f
brayanpena530 <brayanpena530@gmail.com>
2024-09-20 02:50:16 -0500

    added node group for vllm deployment

--- main.tf
+++ main.tf
@@ -111,6 +111,40 @@ module "vpc" {
 }
 
 ################################################################################
+# Node Group Role
+################################################################################
+
+resource "aws_iam_role" "gpu_node_group" {
+  name = "eks-node-gpu-group"
+
+  assume_role_policy = jsonencode({
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+    Version = "2012-10-17"
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "gpu-node-group-AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.gpu_node_group.name
+}
+
+resource "aws_iam_role_policy_attachment" "gpu-node-group-AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.gpu_node_group.name
+}
+
+resource "aws_iam_role_policy_attachment" "gpu-node-group-AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.gpu_node_group.name
+}
+
+################################################################################
 # Cluster
 ################################################################################
 module "eks" {
@@ -161,6 +195,35 @@ module "eks" {
     # (i.e. - at most, only one security group should have this tag in your account)
     "karpenter.sh/discovery" = local.name
   })
+}
+
+resource "aws_eks_node_group" "example" {
+  cluster_name    = module.eks.cluster_name
+  node_group_name = "gpu-nodes"
+  node_role_arn   = aws_iam_role.gpu_node_group.arn
+  subnet_ids      = module.vpc.private_subnets
+
+  scaling_config {
+    desired_size = 1
+    max_size     = 2
+    min_size     = 1
+  }
+
+  # instance_types = ["g5.xlarge"]
+
+  update_config {
+    max_unavailable = 1
+  }
+
+  ami_type = "BOTTLEROCKET_x86_64_NVIDIA"
+
+  # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
+  # Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces.
+  depends_on = [
+    aws_iam_role_policy_attachment.gpu-node-group-AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.gpu-node-group-AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.gpu-node-group-AmazonEC2ContainerRegistryReadOnly,
+  ]
 }
 
 ################################################################################
@@ -251,7 +314,7 @@ module "additional_addons" {
 
   # Install Prometheus and Grafana
   enable_metrics_server        = true
-  enable_kube_prometheus_stack = true
+  enable_kube_prometheus_stack = false
 
   # Disable Prometheus node exporter
   kube_prometheus_stack = {
